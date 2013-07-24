22cbeb67b537e608c9f717d956f75bf8ce8fe0e4
Xavier Subirà <xavier_subira@yahoo.es>
2013-07-24 14:35:38 +0200

    View manager path to layout.

--- config/module.config.php
+++ config/module.config.php
@@ -25,11 +25,21 @@ return array(
 			),
 	),	
     'view_manager' => array(
+        'display_not_found_reason' => true,
+        'display_exceptions'       => true,
+        'doctype'                  => 'HTML5',
+        'not_found_template'       => 'error/404',
+        'exception_template'       => 'error/index',
+        'template_map' => array(
+            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+           // 'album/album/index' => __DIR__ . '/../view/album/album/index.phtml',
+            'error/404'               => __DIR__ . '/../view/error/404.phtml',
+            'error/index'             => __DIR__ . '/../view/error/index.phtml',
+        ),
         'template_path_stack' => array(
             'album' => __DIR__ . '/../view',
         ),
-    ),
-    
+    ),    
     'translator' => array(
         'locale' => 'es_ES',
         'translation_file_patterns' => array(
